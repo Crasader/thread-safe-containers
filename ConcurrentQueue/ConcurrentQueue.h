656a19870a6b522bb76864e7e99c840969968630
Aidan Gallagher <aidan.gallagher@leonardocompany.com>
2020-07-26 00:30:57 +0100

    Rewrite concurrent queue. Remove tests

--- ConcurrentQueue/ConcurrentQueue.h
+++ ConcurrentQueue/ConcurrentQueue.h
@@ -3,148 +3,102 @@
 
 #include <queue>
 #include <shared_mutex>
+#include <condition_variable>
 
-/**
-* @brief Template class wrapper around std::queue to make it thread safe
-* Uses mutexes
-* 
-* Allows multiple consumers and multiple producers to use the queue at the same time
-*
-* Features implemented:
-* - Size
-* - Empty
-* - Pop
-* - TryFront
-* - TryFrontAndPop
-* - Push
-*
-* @note Instances of the class cannot be copied or assigned to another instance
-*/
-enum class QueueStatus 
-{
-    EMPTY,
-    FOUND
-};
-
-template<typename T>
+template<typename T, class Container = std::deque<T>>
 class ConcurrentQueue
 {
+public:
+    ConcurrentQueue() = default;
+    ConcurrentQueue(const ConcurrentQueue& other);
+    ConcurrentQueue<T>& operator=(const ConcurrentQueue& other);
+    ~ConcurrentQueue() = default;
+
+    void push(const T& obj);   
+    T waitingPop();
+    std::pair<bool, T> tryPop();
+    bool empty() const; 
+
 private:
     mutable std::shared_mutex mMutex;
-    std::queue<T> mQueue;
-public:
-    ConcurrentQueue(){};
-    ~ConcurrentQueue(){};
-    ConcurrentQueue(const ConcurrentQueue&) = delete; // Disable copying of the class
-    ConcurrentQueue& operator=(const ConcurrentQueue&) = delete; //Disable assignment of the class
-
-    void push(const T& obj); ///< Add object to the back of the queue
-    void push(T&& obj);      ///< Move object to the back of the queue
-    const QueueStatus getNext(T& obj); ///< Takes in a object and assigns it the first value in the queue, then pops it off the queue
-
-    [[deprecated]] const QueueStatus tryFrontAndPop(T& obj); ///< Takes in a object and assigns it the first value in the queue, then pops it off the queue
-    [[deprecated]]  size_t size() const; ///< Returns the size of the queue
-    bool empty() const; ///< Returns true/false if the queue is empty or not
-    ///@todo Remove this front() function once the everything has been updated to tryFront();
-    [[deprecated]]  const T& front() const; ///< Get a reference to the first item on the queue
-    [[deprecated]]  const QueueStatus tryFront(T& obj) const; ///< Takes in a object and assigns it the first value in the queue
-    [[deprecated]]  void pop(); ///< Pop from the front of the queue
-
-
-    
+    std::queue<T, Container> mQueue;
+    std::condition_variable mConVar;
 };
 
-template<typename T>
-size_t ConcurrentQueue<T>::size() const
+
+
+template<typename T, class Container>
+ConcurrentQueue<T, Container>::ConcurrentQueue(const ConcurrentQueue& other)
 {
-    std::shared_lock<std::shared_mutex> sharedLock(mMutex);
-    return mQueue.size();
+    std::shared_lock<std::shared_mutex> sharedLock(other.mMutex);
+    mQueue = other.mQueue;
 }
 
-template<typename T>
-bool ConcurrentQueue<T>::empty() const
+template<typename T, class Container>
+ConcurrentQueue<T>& ConcurrentQueue<T, Container>::operator=(const ConcurrentQueue& other)
+{
+    // Check not self assigment
+    if (this != &other)
+    {
+        // Lock both mutexes at the same time
+        std::scoped_lock<std::shared_mutex> scoped_lock(mMutex, std::defer_lock);
+        std::shared_lock<std::shared_mutex> other_shared_lock(other.mMutex, std::defer_lock);
+        std::lock(scoped_lock, other_shared_lock);
+
+        mQueue = other.mQueue;
+    }
+    return *this;
+}
+
+template<typename T, class Container>
+void ConcurrentQueue<T, Container>::push(const T& obj)
+{
+    std::unique_lock<std::shared_mutex> uniqueLock(mMutex);
+    bool const was_empty = mQueue.empty();
+
+    mQueue.push(obj);
+
+    // If the queue was empty there may be some consumers waiting for data.
+    if (was_empty)
+    {
+        // Unlock mutex before notifying so waiting threads do not have to wait for the mutex after being woken.
+        uniqueLock.unlock();
+        mConVar.notify_one();
+    }
+}
+
+template<typename T, class Container>
+T ConcurrentQueue<T, Container>::waitingPop()
+{
+    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
+    mConVar.wait(scopedLock, [&this] {return !mQueue.emtpy(); });
+    T val = mQueue.front() //TODO:  value = std::move(data_queue.front()) ?
+    mQueue.pop();
+    return value;
+}
+
+template<typename T, class Container>
+std::pair<bool, T> ConcurrentQueue<T, Container>::tryPop()
+{
+    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
+    if (!mQueue.empty())
+    {
+        T obj = mQueue.front();
+        mQueue.pop();
+        return std::make_pair<true, obj>;
+    }
+    else
+    {
+        T obj; //TODO
+        return std::make_pair<false, obj>
+    }
+}
+
+template<typename T, class Container>
+bool ConcurrentQueue<T, Container>::empty() const
 {
     std::shared_lock<std::shared_mutex> sharedLock(mMutex);
     return mQueue.empty();
-}
-
-template<typename T>
-const T& ConcurrentQueue<T>::front() const
-{
-    std::shared_lock<std::shared_mutex> sharedLock(mMutex);
-    return mQueue.front();    
-}
-
-template<typename T>
-const QueueStatus ConcurrentQueue<T>::tryFront(T& obj) const
-{
-    std::shared_lock<std::shared_mutex> sharedLock(mMutex);
-
-    if (!mQueue.empty())
-    { 
-        obj = mQueue.front();
-        return QueueStatus::FOUND;
-    }  
-    else
-    {
-        return QueueStatus::EMPTY;
-    }  
-}
-
-template<typename T>
-void ConcurrentQueue<T>::pop()
-{
-    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
-    if (!mQueue.empty())
-    { 
-        mQueue.pop();
-    }  
-}
-
-template<typename T>
-const QueueStatus ConcurrentQueue<T>::tryFrontAndPop(T& obj)
-{
-    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
-    if (!mQueue.empty())
-    { 
-        obj = mQueue.front();
-        mQueue.pop();
-        return QueueStatus::FOUND;
-    }   
-    else 
-    {
-        return QueueStatus::EMPTY;
-    }
-}
-
-template<typename T>
-void ConcurrentQueue<T>::push(const T& obj)
-{
-    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
-    mQueue.push(obj);
-}
-
-template<typename T>
-void ConcurrentQueue<T>::push(T&& obj)
-{
-    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
-    mQueue.push(std::move(obj));
-}
-
-template<typename T>
-const QueueStatus ConcurrentQueue<T>::getNext(T& obj)
-{
-    std::scoped_lock<std::shared_mutex> scopedLock(mMutex);
-    if (!mQueue.empty())
-    {
-        obj = mQueue.front();
-        mQueue.pop();
-        return QueueStatus::FOUND;
-    }
-    else
-    {
-        return QueueStatus::EMPTY;
-    }
 }
 
 #endif
